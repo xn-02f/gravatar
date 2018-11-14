6b5728ebd7c4e7b3bbb5f2fb6b104a501d1a83dc
huiyifyj <jxfengyijie@gmail.com>
2018-11-15 01:07:07 +0800

    Add copyright banner in gravatar.js and README.md files

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Gravatar
 > 🖼 `Gravatar` is a library to generate gravatar image url.
+
+<div align=center>
+    &copy; 2018 &nbsp; | &nbsp; <a href="https://xn-02f.github.io" target="_blank">xn-02f</a>
+</div>
--- gravatar.js
+++ gravatar.js
@@ -1,14 +1,25 @@
+/**
+ * This is a file that is used to generate gravatar image url.
+ * (c) 2018 | xn-02f Lab
+ * License: MIT
+ */
+
 const md5 = require('@xn-02f/md5');
 
-const MD5_REGEX = /^[0-9a-f]{32}$/;
+module.exports = (email) => {
 
-const gravatarUrl = 'http://www.gravatar.com/';
+    const gravatarUrl = 'http://www.gravatar.com/';
 
-const md5Hash = (email) => {
-    email = (typeof email === 'string') ? email.trim().toLowerCase() : 'unspecified';
-    return email.match(MD5_REGEX) ? email : md5(email);
+    return gravatarUrl + md5Hash(email);
+
 }
 
-module.exports = (email) => {
-    return gravatarUrl + md5Hash(email);
+const md5Hash = (email) => {
+
+    const MD5_REGEX = /^[0-9a-f]{32}$/;
+
+    email = (typeof email === 'string') ? email.trim().toLowerCase() : 'unspecified';
+
+    return email.match(MD5_REGEX) ? email : md5(email);
+
 }
