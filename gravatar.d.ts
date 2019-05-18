4ed9fa3d2400c3b30bef471b7ee4c962125e0b0d
Pengxn <peng.xn@qq.com>
2019-05-18 14:26:23 +0800

    Create type definition file to support typescript

---
+++ gravatar.d.ts
@@ -0,0 +1,18 @@
+/**
+ * Get the URL to a Gravatar image from an email.
+ *
+ * @param email - Email matching a Gravatar profile.
+ * @example
+ * ```
+ * import gravatarUrl = require('@xn-02f/gravatar');
+ * gravatarUrl('i@example.com', {s: 80});
+ * //=> 'https://www.gravatar.com/avatar/961254e23a4de2aa887ac0f56ef5debf?s=80'
+ * ```
+ */
+
+declare const gravatar = (
+	email: string,
+	options?: object
+) => string;
+
+export = gravatar;
